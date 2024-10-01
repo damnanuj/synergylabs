b68dc04eb931f72488c2642e45d8484f3add99bf
Anuj <anujkrgupta21@gmail.com>
2024-10-01 14:29:25 +0530

    loader fix

--- user-management-app/src/components/userInfo/UserInfo.js
+++ user-management-app/src/components/userInfo/UserInfo.js
@@ -4,10 +4,11 @@ import './UserInfo.scss';
 import { UserContext } from '../../Context/UserContext';
 import profile from "../../assests/profilemale.png"
 import GreenButton from '../common/GreenButton';
+import Loader from '../common/Loader/Loader';
 
 const UserInfo = () => {
   const { id } = useParams(); //==> ID from the URL
-  const { users } = useContext(UserContext);//useCOntext
+  const { users,loading } = useContext(UserContext);//useCOntext
 
 
   //===== Finding the user based on the ID===========
@@ -30,6 +31,14 @@ const UserInfo = () => {
           </div>
         ) : (
           <p style={{textAlign:"center"}}>User not found.</p> //if the user is not found
+        )}
+        {loading && (
+          <>
+            <div className="loading-message">
+              <Loader />
+            </div>
+            <div style={{ height: "100px" }}></div>
+          </>
         )}
       </div>
     </div>
