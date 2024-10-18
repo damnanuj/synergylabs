74aac1f294513fac0107336354c07e6e1623ee2f
Anuj <anujkrgupta21@gmail.com>
2024-10-18 16:30:23 +0530

    join us sectrion

--- bookmycollab-ui-task/src/components/JoinUs/JoinUs.js
+++ bookmycollab-ui-task/src/components/JoinUs/JoinUs.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import "./JoinUs.scss"
+import React from "react";
+import "./JoinUs.scss";
+import { Link } from "react-router-dom";
+import shop from "../../assests/UI-task-images/shop.png";
+import mobile from "../../assests/UI-task-images/mobile.png";
+import social_data from "../../assests/UI-task-images/social_data.png";
+
 const JoinUs = () => {
   return (
-    <div className='joinus_container uni_padding'>
-      <div className='asBrand'></div>
-      <div className='asCreator'></div>
-      <div className='JoinUs_socials'></div>
+    <div className="joinus_container uni_padding">
+      <div className="asBrand">
+        <div className="details">
+          <p>Join As a Brand</p>
+          <h1>
+            Interdum et <br /> malesuada fames ac
+          </h1>
+          <Link to={"/join-as-brand"}>Join as a Brand &#8594;</Link>
+        </div>
+        <img src={shop} alt="shop" />
+      </div>
+      <div className="asCreator">
+        <div className="details">
+          <p>Join As a Creator</p>
+          <h1>
+            Interdum et <br /> malesuada fames ac
+          </h1>
+          <Link to={"/join-as-creator"}>Join As a Creator &#8594;</Link>
+        </div>
+        <img src={mobile} alt="shop" />
+      </div>
+      <div className="JoinUs_socials">
+        <div></div>
+        <div >
+          <img src={social_data} alt="socialData"/>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default JoinUs
+export default JoinUs;
